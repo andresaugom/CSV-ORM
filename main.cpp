d81c8105cae876c44e895a247398ff988e2b1fbd
andresgomez31 <gomez.moreno.andresaugusto.18@gmail.com>
2025-06-19 23:56:09 -0600

    First test. Table class and used classes working

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include "include/csvorm/Table.hpp"
+#include <memory>
+
+int main() {
+    struct Person {
+        Field<int> id;
+        Field<std::string> name;
+        Field<float> score;
+    };
+
+    std::string path = "./data.csv";
+    std::vector<std::shared_ptr<FieldInfoBase>> structure;
+
+    structure.push_back(std::make_shared<FieldInfo<Person, Field<int>>>("id", &Person::id));
+    structure.push_back(std::make_shared<FieldInfo<Person, Field<std::string>>>("id", &Person::name));
+    structure.push_back(std::make_shared<FieldInfo<Person, Field<float>>>("id", &Person::score));
+
+    Table<Person> myTable(structure, path, ',');
+
+    myTable.load();
+
+    myTable.print_vector();
+}
